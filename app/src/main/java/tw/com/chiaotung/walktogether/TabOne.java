22b87216bd3166b76758c792df9447f3694fdd88
Hubert Lee <hub12345@ymail.com>
2016-03-18 20:42:16 +0800

    New Icon,LocalStoreController,First time check
    SignUp Activity has a bug need to be fixed.

--- app/src/main/java/tw/com/chiaotung/walktogether/TabOne.java
+++ app/src/main/java/tw/com/chiaotung/walktogether/TabOne.java
@@ -5,7 +5,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,8 +13,6 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-
-import cc.nctu1210.api.koala3x.SensorEvent;
 
 public class TabOne extends Fragment {
 
@@ -38,7 +35,7 @@ public class TabOne extends Fragment {
 
         //get view block 1
         View UserdataView  = LayoutInflater.from(getActivity()).inflate(R.layout.userdata, null);
-        String useraccount = UserStatus.prefs.getString("account", "");
+        String useraccount = LocalStoreController.userLocalStore.getString("account", "");
         username = (TextView)UserdataView.findViewById(R.id.UserName);
         username.setText(useraccount);
 
--- app/src/main/java/tw/com/chiaotung/walktogether/User.java
+++ app/src/main/java/tw/com/chiaotung/walktogether/User.java
@@ -4,9 +4,14 @@ package tw.com.chiaotung.walktogether;
  * Created by admin on 2016/3/5.
  */
 public class User {
-    String account,password;
+    String account,name,password;
     public User(String account,String password){
         this.account=account;
         this.password=password;
+    }
+    public User(String account,String password,String name){
+        this.account=account;
+        this.password=password;
+        this.name=name;
     }
 }
